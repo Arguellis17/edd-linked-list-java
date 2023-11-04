e45ea60876648e9b6c26626b2d10ae2f41bfc660
Juan Arguello <juandavidaa@ufps.edu.co>
2023-11-04 10:54:17 -0500

    Se agrego completamente la linked list double

--- src/doubles/ClienteMyDoubleLinkedList.java
+++ src/doubles/ClienteMyDoubleLinkedList.java
@@ -26,7 +26,7 @@ public class ClienteMyDoubleLinkedList {
             //listOne.removeBefore(3);
             //listOne.updateNode(5, 1);
             //System.out.println("Existe dentro de la lista?: " + listOne.find(3));
-            listOne.print();
+            listOne.printReverse();
             System.out.println();
         } catch (Exception e) {
             System.err.println("Nodo no valido");
@@ -34,4 +34,34 @@ public class ClienteMyDoubleLinkedList {
 
     }
 
+    public static void main2(String[] args) {
+        
+        // Creando lista enlazada | Con String
+        MyDoubleLinkedList<String> lista = new MyDoubleLinkedList<>();
+        
+        // Agreando elementos a la lista
+        lista.add("Perro");
+        lista.add("Gato");
+        lista.add("Oso");
+        lista.add("Panda");
+
+        // Impresion de la lista
+        lista.print();
+    }
+    
+    public static void main3(String[] args) {
+        
+        // Creando lista enlaza | Con Persona
+      MyDoubleLinkedList<Persona> lista = new MyDoubleLinkedList<>();
+      
+      // Agregando elementos a la lista
+      lista.add(new Persona("Juan", 19));
+      lista.add(new Persona("Diego", 19));
+      lista.add(new Persona("Alex",18));
+      lista.add(new Persona("Romer",20));
+      
+      // Impresion de la lista
+      lista.print();
+        
+    }
 }
